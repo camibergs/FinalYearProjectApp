618a258aa4dd24ebdb8b4cb28e3ea5e48dc8ba8e
Camilla Bergs <camillabergantinos20@yahoo.com>
2024-01-29 17:06:43 +0000

    Small updates Home Screen UI

--- src/components/mainscreens/HomeScreen.js
+++ src/components/mainscreens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Image } from "react-native";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 import { Button, ButtonTray } from "../UI/Button";
 import Icons from "../UI/Icons";
 import ScreenView from "../layout/ScreenView";
@@ -20,31 +20,35 @@ const HomeScreen = ({ navigation }) => {
           style={styles.airport}
         />
         <Text style={styles.text}>Request Special Assistance</Text>
-
-        <View style={styles.container}>
-          <ButtonTray>
-            <Button
-              label="Send Request"
-              icon={<Icons.Form color="white" />}
+        <ButtonTray>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
               style={styles.buttonRequests}
-              onClick={gotoSendRequestScreen}
-            />
-            <Button
-              label="Live Chat"
-              icon={<Icons.LiveChat color="white" />}
+              onPress={gotoSendRequestScreen}
+            >
+              <Icons.Form color="#24325B" />
+              <Text style={styles.label}>Send Request</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
               style={styles.buttonRequests}
               //onClick={gotoLiveChatScreen}
-            />
-            <Button
-              label="Call"
-              icon={<Icons.Call color="white" />}
+            >
+              <Icons.LiveChat color="#24325B" />
+              <Text style={styles.label}>Live Chat</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
               style={styles.buttonRequests}
               //onClick={gotoCallScreen}
-            />
-          </ButtonTray>
-        </View>
+            >
+              <Icons.Call color="#24325B" />
+              <Text style={styles.label}>Call</Text>
+            </TouchableOpacity>
+          </View>
+        </ButtonTray>
 
-        <Text style={styles.text}>Your requests</Text>
+        {/*<Text style={styles.text}>Your requests</Text>*/}
       </View>
     </ScreenView>
   );
@@ -54,6 +58,13 @@ const styles = StyleSheet.create({
   container: {
     padding: 15,
   },
+  buttonContainer: {
+    flexDirection: "row",
+    gap: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
   infoTray: {
     gap: 5,
   },
@@ -61,17 +72,40 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
     fontSize: 24,
     fontWeight: "bold",
+    color: "#24325B",
   },
   text: {
     paddingVertical: 15,
     fontSize: 20,
     fontWeight: "bold",
+    color: "#24325B",
   },
   airport: {
     height: 200,
     width: 400,
     alignItems: "center",
     justifyContent: "center",
+  },
+  buttonTray: {
+    flexDirection: "row",
+    gap: 15,
+  },
+  buttonRequests: {
+    height: 150,
+    width: 122,
+    borderRadius: 15,
+    backgroundColor: "#C0E1EC",
+    alignItems: "center",
+    padding: 10,
+  },
+  label: {
+    marginTop: 50,
+    fontSize: 18,
+    textAlign: "center",
+    flexDirection: "column",
+    justifyContent: "center",
+    color: "#24325B",
+    fontWeight: "bold",
   },
 });
 
