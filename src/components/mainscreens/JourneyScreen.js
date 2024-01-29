bebd6ede704e15b7203770554887e6f051126cfd
Camilla Bergs <camillabergantinos20@yahoo.com>
2024-01-29 12:47:25 +0000

    Created a TrackJourneyScreen and implemented MapView and Marker

--- src/components/mainscreens/JourneyScreen.js
+++ src/components/mainscreens/JourneyScreen.js
@@ -1,14 +1,22 @@
 import { StyleSheet, View, Text } from "react-native";
+import { Button, ButtonTray } from "../UI/Button.js";
+import TrackJourneyScreen from "../screens/TrackJourneyScreen.js";
+
 import ScreenView from "../layout/ScreenView";
 
-const JourneyScreen = () => {
+const JourneyScreen = ({ navigation }) => {
   // Initialisations --------------------------
   // State ------------------------------------
   // Handlers ---------------------------------
+  const gotoTrackJourneyScreen = () =>
+    navigation.navigate("TrackJourneyScreen");
   // View -------------------------------------
   return (
     <ScreenView>
       <Text>Journey Screen</Text>
+      <ButtonTray>
+        <Button label="Track Journey" onClick={gotoTrackJourneyScreen} />
+      </ButtonTray>
     </ScreenView>
   );
 };
